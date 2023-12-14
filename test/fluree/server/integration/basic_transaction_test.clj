adf472dd8d13a5e1d5acaed1310c1fbd38b08e1b
Benjamin Lamothe <bl@732.one>
2023-12-14 15:39:03 -0500

    don't rely on default contexts in basic transaction tests

--- test/fluree/server/integration/basic_transaction_test.clj
+++ test/fluree/server/integration/basic_transaction_test.clj
@@ -1,7 +1,9 @@
 (ns fluree.server.integration.basic-transaction-test
   (:require [clojure.edn :as edn]
-            [clojure.test :refer :all]
-            [fluree.server.integration.test-system :refer :all]
+            [clojure.test :refer [deftest is testing use-fixtures]]
+            [fluree.server.integration.test-system
+             :refer [api-post create-rand-ledger json-headers run-test-server]
+             :as test-system]
             [jsonista.core :as json]))
 
 
@@ -11,11 +13,11 @@
   (testing "can create a new ledger w/ JSON"
     (let [ledger-name (str "create-endpoint-" (random-uuid))
           req         (json/write-value-as-string
-                       {"ledger"   ledger-name
-                        "@context" {"foo" "http://foobar.com/"}
-                        "insert"   [{"id"      "ex:create-test"
-                                     "type"    "foo:test"
-                                     "ex:name" "create-endpoint-test"}]})
+                        {"ledger"   ledger-name
+                         "@context" {"foo" "http://foobar.com/"}
+                         "insert"   [{"id"      "ex:create-test"
+                                      "type"    "foo:test"
+                                      "ex:name" "create-endpoint-test"}]})
           res         (api-post :create {:body req :headers json-headers})]
       (is (= 201 (:status res)))
       (is (= {"ledger" ledger-name
@@ -54,10 +56,11 @@
   (testing "can transact in JSON"
     (let [ledger-name (create-rand-ledger "transact-endpoint-json-test")
           req         (json/write-value-as-string
-                       {"ledger" ledger-name
-                        "insert" {"id"      "ex:transaction-test"
-                                  "type"    "schema:Test"
-                                  "ex:name" "transact-endpoint-json-test"}})
+                        {"@context" test-system/default-context
+                         "ledger" ledger-name
+                         "insert" {"id"      "ex:transaction-test"
+                                   "type"    "schema:Test"
+                                   "ex:name" "transact-endpoint-json-test"}})
           res         (api-post :transact {:body req :headers json-headers})]
       (is (= 200 (:status res)))
       (is (= {"ledger" ledger-name, "t" 2}
@@ -67,27 +70,30 @@
   (testing "can transact with a where clause"
     (let [ledger-name (create-rand-ledger "transact-endpoint-json-test")
           req1        (json/write-value-as-string
-                       {"ledger" ledger-name
-                        "insert" {"id"      "ex:transaction-test"
-                                  "type"    "schema:Test"
-                                  "ex:name" "transact-endpoint-json-test"}})
+                        {"@context" test-system/default-context
+                         "ledger"   ledger-name
+                         "insert"   {"id"      "ex:transaction-test"
+                                     "type"    "schema:Test"
+                                     "ex:name" "transact-endpoint-json-test"}})
           res1        (api-post :transact {:body req1, :headers json-headers})
           _           (assert (= 200 (:status res1)))
           req2        (json/write-value-as-string
-                       {"ledger" ledger-name
-                        "insert" {"id"      "?t"
-                                  "ex:name" "new-name"}
-                        "delete" {"id"      "?t"
-                                  "ex:name" "transact-endpoint-json-test"}
-                        "where"  {"id" "?t", "type" "schema:Test"}})
+                        {"@context" test-system/default-context
+                         "ledger"   ledger-name
+                         "insert"   {"id"      "?t"
+                                     "ex:name" "new-name"}
+                         "delete"   {"id"      "?t"
+                                     "ex:name" "transact-endpoint-json-test"}
+                         "where"    {"id" "?t", "type" "schema:Test"}})
           res2        (api-post :transact {:body req2, :headers json-headers})]
       (is (= 200 (:status res2)))
       (is (= [{"id"      "ex:transaction-test"
                "type"    "schema:Test"
                "ex:name" "new-name"}]
              (->> {:body    (json/write-value-as-string
-                             {"select" {"ex:transaction-test" ["*"]}
-                              "from"   ledger-name})
+                              {"@context" test-system/default-context
+                               "select"   {"ex:transaction-test" ["*"]}
+                               "from"     ledger-name})
                    :headers json-headers}
                   (api-post :query)
                   :body
@@ -95,33 +101,36 @@
   (testing "can transact with a where clause w/ optional"
     (let [ledger-name (create-rand-ledger "transact-endpoint-json-test")
           req1        (json/write-value-as-string
-                       {"ledger" ledger-name
-                        "insert" {"id"          "ex:transaction-test"
-                                  "type"        "schema:Test"
-                                  "schema:name" "transact-endpoint-json-test"}})
+                        {"@context" test-system/default-context
+                         "ledger"   ledger-name
+                         "insert"   {"id"          "ex:transaction-test"
+                                     "type"        "schema:Test"
+                                     "schema:name" "transact-endpoint-json-test"}})
           res1        (api-post :transact {:body req1, :headers json-headers})
           _           (assert (= 200 (:status res1)))
           req2        (json/write-value-as-string
-                       {"ledger" ledger-name
-                        "insert" {"id"          "?t"
-                                  "schema:name" "new-name"}
-                        "delete" {"id"                 "?t"
-                                  "schema:name"        "?n"
-                                  "schema:description" "?d"}
-                        "where"  [{"id" "?t", "type" "schema:Test"}
-                                  ["optional" {"id"          "?t"
-                                               "schema:name" "?n"}]
-                                  ["optional" {"id"                 "?t"
-                                               "schema:description" "?d"}]]})
+                        {"@context" test-system/default-context
+                         "ledger"   ledger-name
+                         "insert"   {"id"          "?t"
+                                     "schema:name" "new-name"}
+                         "delete"   {"id"                 "?t"
+                                     "schema:name"        "?n"
+                                     "schema:description" "?d"}
+                         "where"    [{"id" "?t", "type" "schema:Test"}
+                                     ["optional" {"id"          "?t"
+                                                  "schema:name" "?n"}]
+                                     ["optional" {"id"                 "?t"
+                                                  "schema:description" "?d"}]]})
           res2        (api-post :transact {:body req2, :headers json-headers})]
       (is (= 200 (:status res2)))
       (is (= [{"id"          "ex:transaction-test"
                "type"        "schema:Test"
                "schema:name" "new-name"}]
              (->> {:body    (json/write-value-as-string
-                             {"select" {"?t" ["*"]}
-                              "from"   ledger-name
-                              "where"  {"id" "?t", "type" "schema:Test"}})
+                              {"@context" test-system/default-context
+                               "select"   {"?t" ["*"]}
+                               "from"     ledger-name
+                               "where"    {"id" "?t", "type" "schema:Test"}})
                    :headers json-headers}
                   (api-post :query)
                   :body
@@ -132,10 +141,10 @@
       (let [ledger-name (create-rand-ledger "transact-endpoint-edn-test")
             address     (str "fluree:memory://" ledger-name "/main/head")
             req         (pr-str
-                         {:ledger ledger-name
-                          :txn    [{:id      :ex/transaction-test
-                                    :type    :schema/Test
-                                    :ex/name "transact-endpoint-edn-test"}]})
+                          {:ledger ledger-name
+                           :txn    [{:id      :ex/transaction-test
+                                     :type    :schema/Test
+                                     :ex/name "transact-endpoint-edn-test"}]})
             res         (api-post :transact {:body req :headers edn-headers})]
         (is (= 200 (:status res)))
         (is (= {:address address, :alias ledger-name, :t 2}
