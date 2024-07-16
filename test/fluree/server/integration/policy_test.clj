c8fb73df9dc5e328a5e9a076524f2f897407ddb7
Brian Platz <bplatz@gmail.com>
2024-07-16 09:49:10 -0400

    Updated policy tests

--- test/fluree/server/integration/policy_test.clj
+++ test/fluree/server/integration/policy_test.clj
@@ -45,7 +45,7 @@
                                       {"@id"          "ex:ssnRestriction"
                                        "@type"        ["f:AccessPolicy" "ex:EmployeePolicy"]
                                        "f:onProperty" [{"@id" "schema:ssn"}]
-                                       "f:action"     {"@id" "f:view"}
+                                       "f:action"     [{"@id" "f:view"} {"@id" "f:modify"}]
                                        "f:query"      {"@type"  "@json"
                                                        "@value" {"@context" {"ex" "http://example.org/ns/"}
                                                                  "where"    {"@id"     "?$identity"
@@ -73,119 +73,161 @@
       (is (= [["ex:alice" "111-11-1111"]]
              (-> query-res :body json/read-value))
           "query policy opts should prevent seeing john's ssn")
-      #_(let [txn-req   {:body
+
+      (let [txn-req   {:body
+                       (json/write-value-as-string
+                        {"@context" {"ex"     "http://example.org/ns/"
+                                     "schema" "http://schema.org/"
+                                     "f"      "https://ns.flur.ee/ledger#"}
+                         "ledger"   ledger-name
+                         "delete"   [{"@id"        "ex:alice"
+                                      "schema:ssn" "111-11-1111"}]
+                         "insert"   [{"@id"        "ex:alice"
+                                      "schema:ssn" "222-22-2222"}]
+                         "opts"     {"did" alice-did}})
+                       :headers json-headers}
+            txn-res   (api-post :transact txn-req)
+            _         (assert (= 200 (:status txn-res)))
+            query-req {:body
+                       (json/write-value-as-string
+                        (assoc secret-query
+                          "opts" {"did" alice-did}))
+                       :headers json-headers}
+            query-res (api-post :query query-req)
+            _         (assert (= 200 (:status query-res)))]
+
+        (is (= [["ex:alice" "222-22-2222"]]
+               (-> query-res :body json/read-value))
+            "alice's secret should be modified")
+
+        (testing "plain requests"
+          (let [txn-req {:body
                          (json/write-value-as-string
-                          {"@context" ["https://ns.flur.ee" test-system/default-context]
+                          {"@context" {"ex"     "http://example.org/ns/"
+                                       "schema" "http://schema.org/"
+                                       "f"      "https://ns.flur.ee/ledger#"}
                            "ledger"   ledger-name
-                           "delete"   [{"id"        "ex:alice"
-                                        "ex:secret" "alice's secret"}]
-                           "insert"   [{"id"        "ex:alice"
-                                        "ex:secret" "alice's NEW secret"}]
-                           "opts"     {"role" "ex:userRole"
-                                       "did"  alice-did}})
+                           "insert"   [{"@id"        "ex:john"
+                                        "schema:ssn" "888-88-8888"}]
+                           "opts"     {"did" alice-did}})
                          :headers json-headers}
-              txn-res   (api-post :transact txn-req)
-              _         (assert (= 200 (:status txn-res)))
-              query-req {:body
-                         (json/write-value-as-string
-                          secret-query)
-                         :headers json-headers}
-              query-res (api-post :query query-req)
-              _         (assert (= 200 (:status query-res)))]
-          (is (= #{{"id"        "ex:bob",
-                    "type"      "ex:User",
-                    "ex:secret" "bob's secret"}
-                   {"id"        "ex:alice",
-                    "type"      "ex:User",
-                    "ex:secret" "alice's NEW secret"}}
-                 (-> query-res :body json/read-value set))
-              "alice's secret should be modified")
-          (testing "plain requests"
-            (let [txn-req {:body
-                           (json/write-value-as-string
-                            {"@context" ["https://ns.flur.ee" test-system/default-context]
-                             "ledger"   ledger-name
-                             "insert"   [{"id" "ex:bob"}
-                                         "ex:secret" "bob's new secret"]
-                             "opts"     {"role" "ex:userRole"
-                                         "did"  alice-did}})
-                           :headers json-headers}
-                  txn-res (api-post :transact txn-req)]
-              (is (not= 200 (:status txn-res))
-                  "transaction policy opts prevented modification")
-              (let [query-req {:body
-                               (json/write-value-as-string
-                                {"@context" test-system/default-context
-                                 "from"     ledger-name
-                                 "history"  "ex:bob"
-                                 "t"        {"from" 1}
-                                 "opts"     {"role" "ex:userRole"
-                                             "did"  alice-did}})
-                               :headers json-headers}
-                    query-res (api-post :history query-req)]
-                (is (= 200 (:status query-res)))
-                (is (= [{"id" "ex:bob", "type" "ex:User"}]
-                       (-> query-res :body json/read-value first (get "f:assert")))
-                    "policy opts prevented seeing bob's secret"))))
-          (testing "credential requests"
-            (let [txn-req (<!! (cred/generate
-                                {"@context" ["https://ns.flur.ee" test-system/default-context]
-                                 "ledger"   ledger-name
-                                 "insert"   [{"id" "ex:bob"}
-                                             "ex:secret" "bob's new secret"]}
-                                (:private auth)))
-                  txn-res (api-post :transact {:body    (json/write-value-as-string txn-req)
-                                               :headers json-headers})]
-              (is (not= 200 (:status txn-res))
-                  "transaction policy opts prevented modification")
-              (let [query-req (<!! (cred/generate
-                                    {"@context" test-system/default-context
-                                     "from"     ledger-name
-                                     "history"  "ex:bob"
-                                     "t"        {"from" 1}}
-                                    (:private auth)))
-                    query-res (api-post :history {:body    (json/write-value-as-string query-req)
-                                                  :headers json-headers})]
-                (is (= 200 (:status query-res)))
-                (is (= [{"id" "ex:bob", "type" "ex:User"}]
-                       (-> query-res :body json/read-value first (get "f:assert")))
-                    "policy opts prevented seeing bob's secret"))))
-          (testing "JWS requests"
-            (testing "authorized signer"
-              (let [txn-req {"@context" ["https://ns.flur.ee" test-system/default-context]
-                             "ledger"   ledger-name
-                             "insert"   {"id"        "ex:alice"
-                                         "ex:secret" "Alice's new secret"}}
-                    txn-res (api-post :transact {:body    (crypto/create-jws
-                                                           (json/write-value-as-string txn-req)
-                                                           (:private auth))
-                                                 :headers {"Content-Type" "application/jwt"}})]
-                (is (= 200
-                       (:status txn-res))
-                    "txn signed by authorized user succeeds")))
-            (testing "unauthorized signer"
-              (let [txn-req {"@context" ["https://ns.flur.ee" test-system/default-context]
-                             "ledger"   ledger-name
-                             "insert"   [{"id" "ex:bob"}
-                                         "ex:secret" "bob's new secret"]}
-                    txn-res (api-post :transact {:body    (crypto/create-jws
-                                                           (json/write-value-as-string txn-req)
-                                                           (:private auth))
-                                                 :headers {"Content-Type" "application/jwt"}})]
-                (is (not= 200 (:status txn-res))
-                    "transaction policy opts prevented modification")
-                (is (= 400 (:status txn-res))
-                    "transaction policy opts prevented modification")))
-            (testing "query results filtered based on authorization"
-              (let [query-req {"@context" test-system/default-context
+                txn-res (api-post :transact txn-req)]
+
+            (is (not= 200 (:status txn-res))
+                "transaction policy opts prevented modification")
+
+            (let [query-req {:body
+                             (json/write-value-as-string
+                              {"@context" {"ex"     "http://example.org/ns/"
+                                           "schema" "http://schema.org/"
+                                           "f"      "https://ns.flur.ee/ledger#"}
                                "from"     ledger-name
-                               "history"  "ex:bob"
-                               "t"        {"from" 1}}
-                    query-res (api-post :history {:body    (crypto/create-jws
-                                                            (json/write-value-as-string query-req)
-                                                            (:private auth))
-                                                  :headers {"Content-Type" "application/jwt"}})]
-                (is (= 200 (:status query-res)))
-                (is (= [{"id" "ex:bob", "type" "ex:User"}]
-                       (-> query-res :body json/read-value first (get "f:assert")))
-                    "policy opts prevented seeing bob's secret"))))))))
+                               "history"  "ex:john"
+                               "t"        {"from" 1}
+                               "opts"     {"did" alice-did}})
+                             :headers json-headers}
+                  query-res (api-post :history query-req)]
+
+
+              (is (= 200 (:status query-res)))
+
+              (is (= [{"@id"              "ex:john"
+                       "@type"            "ex:User"
+                       "schema:birthDate" "2021-08-17"
+                       "schema:email"     "john@flur.ee"
+                       "schema:name"      "John"}]
+                     (-> query-res :body json/read-value first (get "f:assert")))
+                  "policy opts prevented seeing john's ssn"))))
+
+        (testing "credential requests"
+          (let [txn-req (<!! (cred/generate
+                              {"@context" {"ex"     "http://example.org/ns/"
+                                           "schema" "http://schema.org/"
+                                           "f"      "https://ns.flur.ee/ledger#"}
+                               "ledger"   ledger-name
+                               "insert"   [{"@id"        "ex:john"
+                                            "schema:ssn" "999-99-9999"}]}
+                              (:private auth)))
+                txn-res (api-post :transact {:body    (json/write-value-as-string txn-req)
+                                             :headers json-headers})]
+
+            (is (not= 200 (:status txn-res))
+                "transaction policy opts prevented modification")
+
+            (let [query-req (<!! (cred/generate
+                                  {"@context" {"ex"     "http://example.org/ns/"
+                                               "schema" "http://schema.org/"
+                                               "f"      "https://ns.flur.ee/ledger#"}
+                                   "from"     ledger-name
+                                   "history"  "ex:john"
+                                   "t"        {"from" 1}}
+                                  (:private auth)))
+                  query-res (api-post :history {:body    (json/write-value-as-string query-req)
+                                                :headers json-headers})]
+
+              (is (= 200 (:status query-res)))
+
+              (is (= [{"@id"              "ex:john"
+                       "@type"            "ex:User"
+                       "schema:birthDate" "2021-08-17"
+                       "schema:email"     "john@flur.ee"
+                       "schema:name"      "John"}]
+                     (-> query-res :body json/read-value first (get "f:assert")))
+                  "policy opts prevented seeing john's ssn"))))
+
+        (testing "JWS requests"
+          (testing "authorized signer"
+            (let [txn-req {"@context" {"ex"     "http://example.org/ns/"
+                                       "schema" "http://schema.org/"
+                                       "f"      "https://ns.flur.ee/ledger#"}
+                           "ledger"   ledger-name
+                           "insert"   {"@id"        "ex:alice"
+                                       "schema:ssn" "444-44-4444"}}
+                  txn-res (api-post :transact {:body    (crypto/create-jws
+                                                         (json/write-value-as-string txn-req)
+                                                         (:private auth))
+                                               :headers {"Content-Type" "application/jwt"}})]
+
+              (is (= 200
+                     (:status txn-res))
+                  "txn signed by authorized user succeeds")))
+
+          (testing "unauthorized signer"
+            (let [txn-req {"@context" {"ex"     "http://example.org/ns/"
+                                       "schema" "http://schema.org/"
+                                       "f"      "https://ns.flur.ee/ledger#"}
+                           "ledger"   ledger-name
+                           "insert"   [{"@id"        "ex:john"
+                                        "schema:ssn" "333-33-3333"}]}
+                  txn-res (api-post :transact {:body    (crypto/create-jws
+                                                         (json/write-value-as-string txn-req)
+                                                         (:private auth))
+                                               :headers {"Content-Type" "application/jwt"}})]
+
+              (is (not= 200 (:status txn-res))
+                  "transaction policy opts prevented modification")
+
+              (is (= 403 (:status txn-res))
+                  "transaction policy opts prevented modification")))
+
+          (testing "query results filtered based on authorization"
+            (let [query-req {"@context" {"ex"     "http://example.org/ns/"
+                                         "schema" "http://schema.org/"
+                                         "f"      "https://ns.flur.ee/ledger#"}
+                             "from"     ledger-name
+                             "history"  "ex:john"
+                             "t"        {"from" 1}}
+                  query-res (api-post :history {:body    (crypto/create-jws
+                                                          (json/write-value-as-string query-req)
+                                                          (:private auth))
+                                                :headers {"Content-Type" "application/jwt"}})]
+
+              (is (= 200 (:status query-res)))
+
+              (is (= [{"@id"              "ex:john"
+                       "@type"            "ex:User"
+                       "schema:birthDate" "2021-08-17"
+                       "schema:email"     "john@flur.ee"
+                       "schema:name"      "John"}]
+                     (-> query-res :body json/read-value first (get "f:assert")))
+                  "policy opts prevented seeing john's secret"))))))))
