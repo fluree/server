8f4a113b8f20bdcf549dbc0d5154d0e5d3f5b07f
Benjamin Lamothe <bl@732.one>
2024-07-19 18:02:08 -0400

    add configuration to start individual servers

--- dev/src/user.clj
+++ dev/src/user.clj
@@ -1,6 +1,7 @@
 (ns user
   (:require [clojure.string :as str]
             [fluree.server.consensus.raft]
+            [fluree.server.system :as system]
             [integrant.repl :refer [clear go halt init reset reset-all]]))
 
 ;; Three Server Configuration
@@ -12,23 +13,33 @@
 (def server-1-overrides
   {:http/jetty        {:port 58090}
    :fluree/connection {:storage-path "data/srv1"}
-   :fluree/consensus  {:servers servers-str
+   :fluree/raft  {:servers servers-str
                        :this-server server-1}})
 
 (def server-2-overrides
   {:http/server       {:port 58091}
    :fluree/connection {:storage-path "data/srv2"}
-   :fluree/consensus  {:consensus-servers     servers-str
+   :fluree/raft  {:consensus-servers     servers-str
                        :consensus-this-server server-2}})
 
 (def server-3-overrides
   {:http/server       {:port 58092}
    :fluree/connection {:storage-path "data/srv3"}
-   :fluree/consensus  {:consensus-servers     servers-str
+   :fluree/raft  {:consensus-servers     servers-str
                        :consensus-this-server server-3}})
 
 (def query-server-1-overrides
   {:http/server {:port 58095}
    :fluree/connection {:method :remote
                        :servers "http://localhost:58090"}
-   :fluree/consensus {:consensus-type :none}})
+   :fluree/raft {:consensus-type :none}})
+
+(defn start-server-1
+  []
+  (system/start :dev server-1-overrides))
+
+(comment
+  (def server-1
+    (start-server-1))
+
+  (system/stop server-1))
