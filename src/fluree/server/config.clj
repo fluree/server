4a06249d7ba07b0893be98bb71718b5e896e752d
Benjamin Lamothe <bl@732.one>
2024-07-22 22:01:38 -0400

    apply overrides set with environment variables

--- src/fluree/server/config.clj
+++ src/fluree/server/config.clj
@@ -1,8 +1,43 @@
 (ns fluree.server.config
   (:require [camel-snake-kebab.core :refer [->kebab-case-keyword]]
             [clojure.java.io :as io]
+            [clojure.walk :as walk]
             [fluree.db.util.json :as json]
             [malli.core :as m]))
+
+
+(def env-template
+  {:fluree {:connection {:remote-servers "FLUREE_REMOTE_SERVERS"
+                         :storage-method "FLUREE_STORAGE_METHOD"
+                         :parallelism    "FLUREE_CONNECTION_PARALLELISM"
+                         :storage-path   "FLUREE_STORAGE_PATH"
+                         :cache-max-mb   "FLUREE_CACHE_MAX_MB"
+                         :defaults       {:index {:reindex-max-bytes "FLUREE_REINDEX_MAX_BYTES"
+                                                  :reindex-min-bytes "FLUREE_REINDEX_MIN_BYTES"}}}
+            :consensus  {:protocol         "FLUREE_CONSENSUS_PROTOCOL"
+                         :max-pending-txns "FLUREE_STANDALONE_MAX_PENDING_TXNS"
+                         :log-history      "FLUREE_RAFT_LOG_HISTORY"
+                         :entries-max      "FLUREE_RAFT_ENTRIES_MAX"
+                         :catch-up-rounds  "FLUREE_RAFT_CATCH_UP_ROUNDS"
+                         :storage-type     "FLUREE_RAFT_STORAGE_TYPE"
+                         :servers          "FLUREE_RAFT_SERVERS"
+                         :this-server      "FLUREE_RAFT_THIS_SERVER"
+                         :log-directory    "FLUREE_RAFT_LOG_DIRECTORY"
+                         :ledger-directory "FLUREE_RAFT_LEDGER_DIRECTORY"}}
+   :http   {:server "FLUREE_HTTP_SERVER"
+            :port   "FLUREE_HTTP_API_PORT"}})
+
+(defn env-config
+  []
+  (walk/postwalk (fn [x]
+                   (cond (string? x)    (System/getenv x)
+                         (map-entry? x) (when (some? (val x))
+                                          x)
+                         (coll? x)      (->> x
+                                             (into (empty x) (remove nil?))
+                                             not-empty)
+                         :else          x))
+                 env-template))
 
 (defn deep-merge
   ([x y]
@@ -23,13 +58,12 @@
 
 (defn load-resource
   ([resource-name]
-   (-> resource-name
-       read-resource
-       (dissoc :profiles)))
+   (load-resource resource-name nil))
 
   ([resource-name profile]
-   (let [config    (read-resource resource-name)
-         overrides (get-in config [:profiles profile])]
+   (let [config            (read-resource resource-name)
+         profile-overrides (get-in config [:profiles profile])
+         env-overrides     (env-config)]
      (-> config
          (dissoc :profiles)
-         (deep-merge overrides)))))
+         (deep-merge profile-overrides env-overrides)))))
