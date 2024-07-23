1b3d41e52733b1a91ad3a36fa0b63c62d524636b
Brian Platz <bplatz@gmail.com>
2024-07-23 05:46:32 -0400

    A bit more flexibility in dev repl

--- dev/src/user.clj
+++ dev/src/user.clj
@@ -12,36 +12,6 @@
             [integrant.core :as ig]
             [integrant.repl :refer [clear go halt init reset reset-all]]))
 
-;; Three Server Configuration
-(def server-1 "/ip4/127.0.0.1/tcp/62071")
-(def server-2 "/ip4/127.0.0.1/tcp/62072")
-(def server-3 "/ip4/127.0.0.1/tcp/62073")
-(def servers-str (str/join "," [server-1 server-2 server-3]))
-
-(def server-1-overrides
-  {:http/jetty        {:port 58090}
-   :fluree/connection {:storage-path "data/srv1"}
-   :fluree/consensus  {:servers servers-str
-                       :this-server server-1}})
-
-(def server-2-overrides
-  {:http/server       {:port 58091}
-   :fluree/connection {:storage-path "data/srv2"}
-   :fluree/consensus  {:consensus-servers     servers-str
-                       :consensus-this-server server-2}})
-
-(def server-3-overrides
-  {:http/server       {:port 58092}
-   :fluree/connection {:storage-path "data/srv3"}
-   :fluree/consensus  {:consensus-servers     servers-str
-                       :consensus-this-server server-3}})
-
-(def query-server-1-overrides
-  {:http/server {:port 58095}
-   :fluree/connection {:method :remote
-                       :servers "http://localhost:58090"}
-   :fluree/consensus {:consensus-type :none}})
-
 (defn load-config
   "Loads aero config at given file path using profile keyword."
   [config-file profile]
@@ -54,15 +24,25 @@
   (ig/expand
    (load-config "config.edn" :dev)))
 
-;; register dev-config as default
-(integrant.repl/set-prep! dev-config)
+;; Register dev-config as the default config
+(def sys-config (dev-config))
+
+(defn set-config!
+  "Sets a new config for use with (go)"
+  [config]
+  (alter-var-root #'sys-config (constantly config)))
+
+(integrant.repl/set-prep! (fn [] sys-config))
 
 
 (defn start!
-  "Starts dev repl"
-  ([] (go))
-  ([overrides]
-   (go (meta-merge overrides))))
+  "Starts dev repl. Optionally provide a config
+  to start with."
+  ([]
+   (go))
+  ([config]
+   (set-config! (ig/expand config))
+   (go)))
 
 (comment
 
