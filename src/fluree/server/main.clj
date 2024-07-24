a97b854b9565a3917a12e3daac578517b1325196
Benjamin Lamothe <bl@732.one>
2024-07-24 15:53:40 -0400

    add command line options parsing

--- src/fluree/server/main.clj
+++ src/fluree/server/main.clj
@@ -1,14 +1,53 @@
 (ns fluree.server.main
-  (:require [fluree.db.util.log :as log]
+  (:require [clojure.tools.cli :as cli]
+            [clojure.string :as str]
+            [fluree.db.util.log :as log]
             [fluree.server.system :as system])
   (:gen-class))
 
 (set! *warn-on-reflection* true)
 
+(def options
+  [["-p" "--profile PROFILE" "Run profile"
+    :parse-fn keyword]
+   ["-c" "--config FILE" "Configuration file path"]
+   ["-h" "--help" "Print this usage summary and exit"]])
+
+(defn usage
+  [summary]
+  (str/join \newline ["Fluree Ledger Server"
+                      ""
+                      "Options:"
+                      summary]))
+
+(defn error-message
+  [errors]
+  (str/join \newline errors))
+
+(defn exit
+  [status message]
+  (println message)
+  (System/exit status))
+
+(defn start-server
+  [{:keys [profile] :as _options}]
+  (if-let [config-path (:config options)]
+    (do (log/info "Starting Fluree server configuration at path:" config-path
+                  "with profile:" profile)
+        (system/start-file config-path profile))
+    (do (log/info "Starting Fluree server with profile:" profile)
+        (system/start profile))))
+
 (defn -main
   [& args]
-  (let [first-arg (first args)
-        profile   (or (keyword first-arg)
-                      :prod)]
-    (log/info "Starting fluree/server with profile:" profile)
-    (system/start profile)))
+  (let [{:keys [options errors summary]} (cli/parse-opts args options)]
+    (cond (seq errors)
+          (let [msg (error-message errors)]
+            (exit 1 msg))
+
+          (:help options)
+          (let [msg (usage summary)]
+            (exit 0 msg))
+
+          :else
+          (start-server options))))
