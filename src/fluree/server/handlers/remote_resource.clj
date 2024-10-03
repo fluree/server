8dfb6d9ab0b0e6b062a4db1166d1cb7ea79af98f
Benjamin Lamothe <bl@732.one>
2024-10-02 20:49:12 -0400

    read local commits in response to remote requests

--- src/fluree/server/handlers/remote_resource.clj
+++ src/fluree/server/handlers/remote_resource.clj
@@ -9,7 +9,7 @@
   [{:keys [fluree/conn]
     {{ledger-address :resource :as body} :body} :parameters}]
   (log/debug "Remote resource read request:" body)
-  (let [result (<?? (connection/read-latest-commit conn ledger-address))]
+  (let [result (<?? (connection/read-latest-local-commit conn ledger-address))]
     {:status 200
      :body   result}))
 
