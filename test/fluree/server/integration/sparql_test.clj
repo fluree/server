a965df0a155a010b8c803cc477f467b2fe305872
Daniel Petranek <dpetranek@flur.ee>
2025-02-19 16:53:38 -0600

    add support for application/sparql-results+json output
    per https://www.w3.org/TR/2013/REC-sparql11-protocol-20130321/#conneg

--- test/fluree/server/integration/sparql_test.clj
+++ test/fluree/server/integration/sparql_test.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test :refer [deftest is testing use-fixtures]]
             [fluree.server.integration.test-system
              :refer [api-post create-rand-ledger json-headers run-test-server
-                     sparql-headers]]
+                     sparql-results-headers]]
             [jsonista.core :as json]))
 
 (use-fixtures :once run-test-server)
@@ -10,27 +10,28 @@
 (deftest ^:integration ^:sparql query-sparql-test
   (testing "basic SPARQL query works"
     (let [ledger-name (create-rand-ledger "query-sparql-test")
-          txn-req     {:body
-                       (json/write-value-as-string
-                        {"ledger"   ledger-name
-                         "@context" {"schema" "http://schema.org/"
-                                     "ex"     "http://example.org/"}
-                         "insert"   [{"@id"     "ex:query-sparql-test"
-                                      "@type"   "schema:Test"
-                                      "ex:name" "query-sparql-test"}]})
-                       :headers json-headers}
-          txn-res     (api-post :transact txn-req)
+          txn         {"ledger"   ledger-name
+                       "@context" {"schema" "http://schema.org/"
+                                   "ex"     "http://example.org/"}
+                       "insert"   [{"@id"     "ex:query-sparql-test"
+                                    "@type"   "schema:Test"
+                                    "ex:name" "query-sparql-test"}]}
+          txn-res     (api-post :transact {:body    (json/write-value-as-string txn)
+                                           :headers json-headers})
           _           (assert (= 200 (:status txn-res)))
-          query-req   {:body    (str "PREFIX schema: <http://schema.org/>
+          query       (str "PREFIX schema: <http://schema.org/>
                                       PREFIX ex: <http://example.org/>
                                       SELECT ?name
                                       FROM   <" ledger-name ">
                                       WHERE  {?test a schema:Test;
                                                     ex:name ?name.}")
-                       :headers sparql-headers}
-          query-res   (api-post :query query-req)]
+          query-res   (api-post :query {:body    query
+                                        :headers sparql-results-headers})]
 
       (is (= 200 (:status query-res)))
 
-      (is (= [["query-sparql-test"]]
+      (is (= {"head" {"vars" ["name"]}
+              "results"
+              {"bindings"
+               [{"name" {"value" "query-sparql-test", "type" "literal"}}]}}
              (-> query-res :body json/read-value))))))
