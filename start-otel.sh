5b7dab9e72e46f5fa9f6b03045c50c2d3b215766
Kurt Harriger <kharriger@flur.ee>
2025-02-27 08:43:13 -0700

    start-otel repl and logger options

--- start-otel.sh
+++ start-otel.sh
@@ -9,18 +9,53 @@ if [ ! -f opentelemetry-javaagent.jar ]; then
 fi
 
 export JAVA_TOOL_OPTIONS="-javaagent:opentelemetry-javaagent.jar"
+
 export OTEL_SERVICE_NAME="fluree-server"
 export OTEL_RESOURCE_ATTRIBUTES=service.namespace=fluree
 
 export OTEL_TRACES_SAMPLER="always_on"
-# export OTEL_EXPORTER_OTLP_ENDPOINT="http://localhost:4318"
 export OTEL_INSTRUMENTATION_LOGBACK_APPENDER_EXPERIMENTAL_CAPTURE_MDC_ATTRIBUTES="*"
 export OTEL_RESOURCE_PROVIDERS_AWS_ENABLED="true"
 # this tells the otel collector to use X-Amzn-Trace-Id request header as trace and span id
 export export OTEL_PROPAGATORS=xray
 
-export JSON_LOGGING=${JSON_LOGGING:-false}
-export FLUREE_LOG_LEVEL=${FLUREE_LOG_LEVEL:-debug}
+export CONSOLE_APPENDER=${CONSOLE_APPENDER:-CONSOLE_COLORS}
+export ROOT_LOG_LEVEL=${ROOT_LOG_LEVEL:-INFO}
+export FLUREE_LOG_LEVEL=${FLUREE_LOG_LEVEL:-INFO}
+
+is_port_in_use() {
+  local port=$1  # Take the first argument as the port number
+
+  if command -v lsof &> /dev/null; then
+    lsof -i :$port > /dev/null
+  elif command -v ss &> /dev/null; then
+    ss -tuln | grep ":$port" > /dev/null
+  else
+    echo "Error: No command found to check if ports is listening (lsof, or ss)" >&2
+    return 0
+  fi
+}
+
+# default endpoint is http://localhost:4318 with expectation that a sidecar
+# is listening. It can still be useful to enable instrumentation locally
+# to get trace ids in logs even if nothing is listening
+# export OTEL_EXPORTER_OTLP_ENDPOINT="http://localhost:4318"
+if [ -z "$OTEL_EXPORTER_OTLP_ENDPOINT" ] && ! is_port_in_use 4318; then
+    # instrument app for structured logging, but don't actually export the data
+    echo "Instrumentation is enabled but nothing is listening, exporters will be disabled"
+    export OTEL_TRACES_EXPORTER="none"
+    export OTEL_METRICS_EXPORTER="none"
+    export OTEL_LOGS_EXPORTER="none"
+fi
 
 clojure -X:deps prep
-clojure -X:run-dev
+
+if [ "$1" == "repl" ]; then
+    export PROFILES=${PROFILES:-":dev:dev/reloaded:run-dev:test"}
+    if [ -n "$2" ]; then
+      PROFILES="$2"
+    fi
+    clojure -Sdeps '{:deps {nrepl/nrepl {:mvn/version,"1.1.0"},cider/cider-nrepl {:mvn/version,"0.45.0"}}}' -M:$PROFILES -m nrepl.cmdline --middleware "[cider.nrepl/cider-middleware]"
+else
+    clojure -X:run-dev
+fi
