f3826726fd0294a3bfc24bba7c6dc7f5f1b58bab
Benjamin Lamothe <bl@732.one>
2025-03-03 15:48:28 -0500

    use keyword namespace aliases

--- src/fluree/server/system.clj
+++ src/fluree/server/system.clj
@@ -1,69 +1,73 @@
 (ns fluree.server.system
-  (:require [fluree.db.connection.config :as conn-config]
+  (:require [fluree.db :as-alias db]
+            [fluree.db.connection.config :as conn-config]
             [fluree.db.connection.system :as conn-system]
             [fluree.db.connection.vocab :as conn-vocab]
             [fluree.db.util.core :as util :refer [get-first]]
+            [fluree.server :as-alias server]
             [fluree.server.broadcast.subscriptions :as subscriptions]
             [fluree.server.config :as config]
             [fluree.server.config.vocab :as server-vocab]
+            [fluree.server.consensus :as-alias consensus]
             [fluree.server.consensus.raft :as raft]
             [fluree.server.consensus.standalone :as standalone]
             [fluree.server.consensus.watcher :as watcher]
             [fluree.server.handler :as handler]
+            [fluree.server.http :as-alias http]
             [fluree.server.task.migrate-sid :as task.migrate-sid]
             [integrant.core :as ig]
             [ring.adapter.jetty9 :as jetty]))
 
 (set! *warn-on-reflection* true)
 
-(derive :fluree.server.consensus/raft :fluree.server/consensus)
-(derive :fluree.server.consensus/standalone :fluree.server/consensus)
-(derive :fluree.server/subscriptions :fluree.server/broadcast)
-(derive :fluree.server.http/jetty :fluree.server/http)
+(derive ::consensus/raft ::server/consensus)
+(derive ::consensus/standalone ::server/consensus)
+(derive ::server/subscriptions ::server/broadcast)
+(derive ::http/jetty ::server/http)
 
-(defmethod ig/expand-key :fluree.server/http
+(defmethod ig/expand-key ::server/http
   [k config]
   (let [max-txn-wait-ms (conn-config/get-first-integer config server-vocab/max-txn-wait-ms)
         closed-mode     (conn-config/get-first-boolean config server-vocab/closed-mode)
         root-identities (set (conn-config/get-strings config server-vocab/root-identities))
         config*         (-> config
-                            (assoc :handler (ig/ref :fluree.server.api/handler))
+                            (assoc :handler (ig/ref ::server/handler))
                             (dissoc server-vocab/max-txn-wait-ms server-vocab/closed-mode
                                     server-vocab/root-identities))]
-    {k                          config*
-     :fluree.server/watcher     {:max-txn-wait-ms max-txn-wait-ms}
-     :fluree.server.api/handler {:root-identities root-identities
-                                 :closed-mode     closed-mode
-                                 :connection      (ig/ref :fluree.db/connection)
-                                 :consensus       (ig/ref :fluree.server/consensus)
-                                 :watcher         (ig/ref :fluree.server/watcher)
-                                 :subscriptions   (ig/ref :fluree.server/subscriptions)}}))
+    {k                config*
+     ::server/watcher {:max-txn-wait-ms max-txn-wait-ms}
+     ::server/handler {:root-identities root-identities
+                       :closed-mode     closed-mode
+                       :connection      (ig/ref ::db/connection)
+                       :consensus       (ig/ref ::server/consensus)
+                       :watcher         (ig/ref ::server/watcher)
+                       :subscriptions   (ig/ref ::server/subscriptions)}}))
 
-(defmethod ig/expand-key :fluree.server.consensus/standalone
+(defmethod ig/expand-key ::consensus/standalone
   [k config]
   {k (assoc config
-            :broadcaster (ig/ref :fluree.server/broadcast)
-            :watcher (ig/ref :fluree.server/watcher))})
+            :broadcaster (ig/ref ::server/broadcast)
+            :watcher (ig/ref ::server/watcher))})
 
-(defmethod ig/init-key :fluree.server/subscriptions
+(defmethod ig/init-key ::server/subscriptions
   [_ _]
   (subscriptions/listen))
 
-(defmethod ig/halt-key! :fluree.server/subscriptions
+(defmethod ig/halt-key! ::server/subscriptions
   [_ subs]
   (subscriptions/close subs))
 
-(defmethod ig/init-key :fluree.server/watcher
+(defmethod ig/init-key ::server/watcher
   [_ {:keys [max-txn-wait-ms]}]
   (if max-txn-wait-ms
     (watcher/start max-txn-wait-ms)
     (watcher/start)))
 
-(defmethod ig/halt-key! :fluree.server/watcher
+(defmethod ig/halt-key! ::server/watcher
   [_ watcher]
   (watcher/stop watcher))
 
-(defmethod ig/init-key :fluree.server.consensus/raft
+(defmethod ig/init-key ::consensus/raft
   [_ config]
   (let [log-history      (conn-config/get-first-integer config server-vocab/log-history)
         entries-max      (conn-config/get-first-integer config server-vocab/entries-max)
@@ -80,37 +84,37 @@
                  :log-directory    log-directory
                  :ledger-directory ledger-directory})))
 
-(defmethod ig/halt-key! :fluree.server.consensus/raft
+(defmethod ig/halt-key! ::consensus/raft
   [_ {:keys [close] :as _raft-group}]
   (close))
 
-(defmethod ig/init-key :fluree.server.consensus/standalone
+(defmethod ig/init-key ::consensus/standalone
   [_ {:keys [broadcaster watcher] :as config}]
   (let [max-pending-txns (conn-config/get-first-integer config server-vocab/max-pending-txns)
         conn             (get-first config conn-vocab/connection)]
     (standalone/start conn broadcaster watcher max-pending-txns)))
 
-(defmethod ig/halt-key! :fluree.server.consensus/standalone
+(defmethod ig/halt-key! ::consensus/standalone
   [_ transactor]
   (standalone/stop transactor))
 
-(defmethod ig/init-key :fluree.server.api/handler
+(defmethod ig/init-key ::server/handler
   [_ config]
   (-> config
       (select-keys [:connection :consensus :watcher :subscriptions :root-identities
                     :closed-mode])
       handler/app))
 
-(defmethod ig/init-key :fluree.server.http/jetty
+(defmethod ig/init-key ::http/jetty
   [_ {:keys [handler] :as config}]
   (let [port (conn-config/get-first-integer config server-vocab/http-port)]
     (jetty/run-jetty handler {:port port, :join? false})))
 
-(defmethod ig/halt-key! :fluree.server.http/jetty
+(defmethod ig/halt-key! ::http/jetty
   [_ http-server]
   (jetty/stop-server http-server))
 
-(defmethod ig/init-key :fluree.server/sid-migration
+(defmethod ig/init-key ::server/sid-migration
   [_ {:keys [conn ledgers force]}]
   (task.migrate-sid/migrate conn ledgers force))
 
