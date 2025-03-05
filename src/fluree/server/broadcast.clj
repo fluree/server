fccf5149ad0c2533ea4701fda626886d4eaa2ce6
Benjamin Lamothe <bl@732.one>
2025-03-05 08:09:48 -0500

    separate methods for broadcasting commits and errors

--- src/fluree/server/broadcast.clj
+++ src/fluree/server/broadcast.clj
@@ -3,19 +3,20 @@
 (set! *warn-on-reflection* true)
 
 (defprotocol Broadcaster
-  (-broadcast [b ledger-id event]))
+  (-broadcast-commit [b ledger-id event])
+  (-broadcast-error [b ledger-id error-event]))
 
 (defn broadcast-new-ledger!
   [broadcaster {:keys [ledger-id] :as ledger-created-event}]
-  (-broadcast broadcaster ledger-id ledger-created-event)
+  (-broadcast-commit broadcaster ledger-id ledger-created-event)
   ::new-ledger)
 
 (defn broadcast-new-commit!
   [broadcaster {:keys [ledger-id] :as transaction-committed-event}]
-  (-broadcast broadcaster ledger-id transaction-committed-event)
+  (-broadcast-commit broadcaster ledger-id transaction-committed-event)
   ::new-commit)
 
 (defn broadcast-error!
   [broadcaster {:keys [ledger-id] :as error-event}]
-  (-broadcast broadcaster ledger-id error-event)
+  (-broadcast-error broadcaster ledger-id error-event)
   ::error)
