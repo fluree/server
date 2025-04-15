25014734b093a9f04f9c893b6f6c4263190b33bd
Daniel Petranek <dpetranek@flur.ee>
2025-04-15 15:08:48 -0500

    update db to fix stale policy wrapping

--- test/fluree/server/integration/load_state_test.clj
+++ test/fluree/server/integration/load_state_test.clj
@@ -1,4 +1,4 @@
-(ns fluree.server.integration.shacl-test
+(ns fluree.server.integration.load-state-test
   (:require [clojure.test :refer [deftest is testing]]
             [fluree.server.integration.test-system :refer [api-post json-headers] :as test-system]
             [fluree.server.system :as system]
@@ -37,4 +37,48 @@
           (is (= 422
                  (-> (api-post :transact {:body (json/write-value-as-string txn-req) :headers json-headers})
                      :status)))
+          (system/stop new-server))))))
+
+(deftest ^:integration policy-test
+  (test-system/set-server-ports)
+  (with-tmp-dir storage-path
+    (let [server (system/start-config (test-system/file-server-config storage-path))
+          create {"ledger" "user/ledger",
+                  "insert" {"@id"      "freddy",
+                            "@type"    "Yeti",
+                            "name"     "Freddy",
+                            "age"      4,
+                            "verified" true},
+                  "opts"
+                  {"policy" {"@type"                            ["https://ns.flur.ee/ledger#AccessPolicy"],
+                             "https://ns.flur.ee/ledger#action" {"@id" "https://ns.flur.ee/ledger#modify"},
+                             "https://ns.flur.ee/ledger#query"  {"@type"  "@json",
+                                                                 "@value" {}}}}}
+
+          query {"from"   "user/ledger",
+                 "where"  [{"@id" "?s", "age" "?age"}],
+                 "select" {"?s" ["*"]}}]
+      (testing "validation works on initial server run"
+        (is (= 201
+               (-> (api-post :create {:body (json/write-value-as-string create) :headers json-headers})
+                   :status)))
+        (is (= [{"@id"      "freddy",
+                 "age"      4,
+                 "name"     "Freddy",
+                 "verified" true,
+                 "@type"    "Yeti"}]
+               (-> (api-post :query {:body (json/write-value-as-string query) :headers json-headers})
+                   :body
+                   json/read-value))))
+      (testing "validation works on server restart"
+        (system/stop server)
+        (let [new-server (system/start-config (test-system/file-server-config storage-path))]
+          (is (= [{"@id"      "freddy",
+                   "age"      4,
+                   "name"     "Freddy",
+                   "verified" true,
+                   "@type"    "Yeti"}]
+                 (-> (api-post :query {:body (json/write-value-as-string query) :headers json-headers})
+                     :body
+                     json/read-value)))
           (system/stop new-server))))))
