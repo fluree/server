a327d1891650c151e5d566f69615737ab0d58f25
Kurt Harriger <kurtharriger@gmail.com>
2025-04-18 16:56:33 -0600

    push release when tagged

--- build.clj
+++ build.clj
@@ -3,7 +3,9 @@
             [org.corfield.build :as bb]))
 
 (def lib 'fluree/server)
-(def version (format "0.1.%s" (b/git-count-revs nil)))
+(def version
+  (or (System/getenv "VERSION")
+      (format "0.1.%s" (b/git-count-revs nil))))
 
 (defn uber [_]
   (bb/uber {:lib     lib
