bea29f0d55b8d57d6f07fa3e77545ab320ad520d
Daniel Petranek <dpetranek@flur.ee>
2025-06-18 13:31:15 -0500

    use same alias for fluree.server.handlers.transact ns everywhere

--- src/fluree/server/handlers/create.clj
+++ src/fluree/server/handlers/create.clj
@@ -3,11 +3,7 @@
             [fluree.db.util.log :as log]
             [fluree.server.consensus :as consensus]
             [fluree.server.handlers.shared :as shared :refer [deref! defhandler]]
-            [fluree.server.handlers.transact :refer [derive-tx-id
-                                                     commit-event->response-body
-                                                     extract-ledger-id
-                                                     monitor-consensus-persistence
-                                                     monitor-commit]]
+            [fluree.server.handlers.transact :as srv-tx]
             [fluree.server.watcher :as watcher]))
 
 (set! *warn-on-reflection* true)
@@ -16,15 +12,15 @@
   [consensus watcher ledger tx-id txn opts]
   (let [;; initial response is not completion, but acknowledgement of persistence
         persist-resp-ch (consensus/queue-new-ledger consensus ledger tx-id txn opts)]
-    (monitor-consensus-persistence watcher ledger tx-id persist-resp-ch)))
+    (srv-tx/monitor-consensus-persistence watcher ledger tx-id persist-resp-ch)))
 
 (defn create-ledger!
   [consensus watcher ledger-id txn opts]
   (let [p         (promise)
-        tx-id     (derive-tx-id txn)
+        tx-id     (srv-tx/derive-tx-id txn)
         result-ch (watcher/create-watch watcher tx-id)]
     (queue-consensus consensus watcher ledger-id tx-id txn opts)
-    (monitor-commit p ledger-id tx-id result-ch)
+    (srv-tx/monitor-commit p ledger-id tx-id result-ch)
     p))
 
 (defhandler default
@@ -33,7 +29,7 @@
   (log/debug "create body:" body)
   (let [txn          (fluree/format-txn body opts)
         ledger-id    (or (:ledger opts)
-                         (extract-ledger-id txn))
+                         (srv-tx/extract-ledger-id txn))
         opts*        (dissoc opts :identity) ; Remove identity option because the
                                              ; request should have been validated
                                              ; upstream, and there are no policies
@@ -41,6 +37,6 @@
                                              ; actions
         commit-event (deref! (create-ledger! consensus watcher ledger-id txn opts*))
 
-        body (commit-event->response-body commit-event)]
+        body (srv-tx/commit-event->response-body commit-event)]
     (shared/with-tracking-headers {:status 201, :body body}
       commit-event)))
